a0bbc1711ab2d0f47bda49d4325b9fa9aebdd959
walter2190 <81540031+walter2190@users.noreply.github.com>
2021-05-07 21:59:02 -0500

    Revert "Cow sv 01 inicio sesion"

--- main/WpfApp1/MainWindow.xaml.cs
+++ main/WpfApp1/MainWindow.xaml.cs
@@ -20,49 +20,15 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> usuario = new List<string>();
-        List<string> contrasena = new List<string>();
-        int aux = 0;
-       
         public MainWindow()
         {
             InitializeComponent();
             //asdasd
         }
 
-        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            int i = 0, ban = 0;
-            while (i < aux)
-            {
-                if (usuario[i] == txtUsuario.Text && contrasena[i] == txtContrasena.Text)
-                {
-                    Principal ventanaprincipal = new Principal(usuario[i]);
-                    ventanaprincipal.Show();
-                    this.Close();
-                    ban = 1;
-                }
-                i++;
-            }
-            if (ban == 0)
-            {
-                MessageBox.Show("Dato no encontrado...");
-            }
-        }
 
-
-        private void btnRegistrarse_Click(object sender, RoutedEventArgs e)
-        {
-            int i = 0;
-            for (i = 0; i <= aux; i++)
-            {
-                usuario.Add(txtUsuario.Text);
-                contrasena.Add(txtContrasena.Text);
-                txtUsuario.Text = "";
-                txtContrasena.Text = "";
-            }
-            aux = aux + 1;
-            MessageBox.Show("Usuario registrado en la posicion: " + aux);
         }
     }
 }
